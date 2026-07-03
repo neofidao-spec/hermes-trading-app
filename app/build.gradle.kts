cd03574fadc7456c812142729bd8fc4a1ac1eab7
Fani Hernanda <fani@trader.local>
2026-07-03 23:55:54 +0800

    fix: remove deprecated okhttp-ws dependency (WebSocket built-in in OkHttp 4.x)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.squareup.okhttp3:okhttp-ws:0.3.2")
     
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
